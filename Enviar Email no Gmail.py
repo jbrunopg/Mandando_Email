6ca27a739f7444deeb2d417be8f05b3eebe945c1
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-23 23:54:52 -0300

    Create Enviar Email no Gmail.py

---
+++ Enviar Email no Gmail.py
@@ -0,0 +1,11 @@
+import smtplib
+import email.message
+
+def enviar_email():
+    corpo_email = 
+    <p>Parágrafo1</p>
+    <p>Parágrafo2</p>
+    
+    
+    msg = email.message.Message()
+    msg['Subject'] = 'Assunto'
